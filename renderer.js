55a8c6186985c82df600d13f9e14a1940a55a267
Estelle Doreau <doreau.e@gmail.com>
2018-05-26 19:32:17 +0200

    first commit

---
+++ renderer.js
@@ -0,0 +1,9 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+// const request = require('request');
+// request('http://www.google.com', function (error, response, body) {
+//     // Affiche la page d'accueil de Google dans l'application
+//     document.querySelector('body').innerHTML = body;
+// });
